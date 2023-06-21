c59209cd766d90544de67be0103da8fe5ac409c6
Bruno L. Alves <bruno_lima011@outlook.com>
2023-06-21 03:46:54 -0300

    Exercícios 21/06

--- Exercícios/21 - Tocando um MP3.py
+++ Exercícios/21 - Tocando um MP3.py
@@ -1,1 +1,6 @@
 import pygame
+pygame.init()
+print('Tocando música')
+pygame.mixer.music.load('ex.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
---
+++ Exercícios/26 – Primeira e última ocorrência de uma string.py
@@ -0,0 +1,4 @@
+frase = str(input('Digite uma frase: ')).split()
+print('A letra {} aparece {} vezes na frase')
+print('A primeira letra {} apareceu na posição {}')
+print('A última letra {} apareceu na posição {}')
